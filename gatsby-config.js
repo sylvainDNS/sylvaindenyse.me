e5367292731356d99df4199461fe991f668fe782
Sylvain DNS <sylvain.denyse@gmail.com>
2020-12-30 22:52:51 +0100

    build: use new image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,8 @@ module.exports = {
   plugins: [
     'gatsby-plugin-eslint',
     'gatsby-plugin-emotion',
+    'gatsby-plugin-image',
     'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: path.join(__dirname, 'src', 'images'),
-      },
-    },
   ],
 }
--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,35 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql, useStaticQuery } from 'gatsby'
-import GatsbyImage from 'gatsby-image'
+import { StaticImage } from 'gatsby-plugin-image'
 import { css } from '@emotion/react'
 
-const Avatar = ({ css: cssProps }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      file(relativePath: { eq: "avatar.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 200) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-
-  return (
-    <GatsbyImage
-      css={css`
-        border-radius: 50%;
-        width: 100%;
-        max-width: min(30%, 200px);
-        ${cssProps};
-      `}
-      fluid={data.file.childImageSharp.fluid}
-      alt="A picture of me"
-    />
-  )
-}
+const Avatar = ({ css: cssProps }) => (
+  <StaticImage
+    css={css`
+      border-radius: 50%;
+      ${cssProps};
+    `}
+    width={200}
+    src="../images/avatar.jpg"
+    alt="A picture of me"
+  />
+)
 
 Avatar.propTypes = {
   css: PropTypes.object,

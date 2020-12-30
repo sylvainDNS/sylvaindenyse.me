5c1a130bcbfde667db9aac4d3ee4090dc0ea4cf2
Sylvain DNS <sylvain.denyse@gmail.com>
2020-12-30 23:51:16 +0100

    feat: add manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,22 @@ module.exports = {
     'gatsby-plugin-eslint',
     'gatsby-plugin-emotion',
     'gatsby-plugin-image',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Sylvain DENYSE - Web Developer Designer',
+        short_name: 'Sylvain DENYSE',
+        start_url: '/',
+        background_color: '#eff4ff',
+        theme_color: '#eff4ff',
+        display: 'standalone',
+        icon: 'src/images/icon.png',
+        icon_options: {
+          purpose: `maskable`,
+        },
+        crossOrigin: `use-credentials`,
+      },
+    },
     'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
     'gatsby-plugin-sitemap',

5c878667a31cdb895f1ba5d5ee88cf757511e778
Sylvain DNS <sylvain.denyse@gmail.com>
2020-12-30 23:30:37 +0100

    feat: add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
 module.exports = {
+  siteMetadata: {
+    siteUrl: `https://sylvaindenyse.me`,
+  },
   plugins: [
     'gatsby-plugin-eslint',
     'gatsby-plugin-emotion',
     'gatsby-plugin-image',
     'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
   ],
 }

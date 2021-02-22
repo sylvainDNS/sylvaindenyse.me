bfc46f31db3e2672fb376d6a29b31cde14739924
Sylvain DNS <sylvain.denyse@gmail.com>
2021-02-22 21:10:45 +0100

    feat: add CNAME record

--- src/components/seo.js
+++ src/components/seo.js
@@ -54,7 +54,7 @@ const SEO = ({
         async
         defer
         data-domain="sylvaindenyse.me"
-        src="https://plausible.io/js/plausible.js"
+        src="https://stats.sylvaindenyse.me/js/index.js"
       ></script>
     </Helmet>
   )

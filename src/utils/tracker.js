9213c6a74c8662ca211602c2d25ea61fa17adb8d
Sylvain DNS <sylvain.denyse@gmail.com>
2021-03-01 12:47:15 +0100

    fix: custom api host

--- src/utils/tracker.js
+++ src/utils/tracker.js
@@ -2,7 +2,7 @@ import Plausible from 'plausible-tracker'
 
 const plausible = Plausible({
   domain: 'sylvaindenyse.me',
-  apiHost: 'stats.sylvaindenyse.me',
+  apiHost: 'https://stats.sylvaindenyse.me',
 })
 
 export default plausible

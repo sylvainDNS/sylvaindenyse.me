76f62665359ef492a0a352d575128a3c4facde6c
Sylvain DNS <sylvain.denyse@gmail.com>
2021-03-12 00:51:36 +0100

    feat: create blog layout & blog post processing

---
+++ gatsby-node.js
@@ -0,0 +1,32 @@
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+
+  const blogPostTemplate = require.resolve('./src/templates/blog.js')
+
+  const result = await graphql(`
+    {
+      allMdx(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
+        nodes {
+          frontmatter {
+            path
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild('Error while running GraphQL query.')
+    return
+  }
+
+  result.data.allMdx.nodes.forEach(({ frontmatter: { path } }) => {
+    createPage({
+      path,
+      component: blogPostTemplate,
+      context: {
+        slug: path,
+      },
+    })
+  })
+}
---
+++ src/templates/blog.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import PropTypes from 'prop-types'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import styled from '@emotion/styled'
+import SEO from '../components/seo'
+
+const getTimeToRead = (words, wordsPerMinute = 180) =>
+  Math.round(words / wordsPerMinute) || 1
+
+const Article = styled.article`
+  .title {
+    margin-bottom: 0;
+  }
+
+  .publication-hint {
+    font-style: italic;
+    font-weight: var(--font-weight-light);
+  }
+
+  p:first-of-type {
+    margin-top: 2rem;
+  }
+`
+
+const Template = ({ data }) => {
+  const { mdx } = data
+  const {
+    frontmatter,
+    body,
+    wordCount: { words },
+    excerpt,
+  } = mdx
+  const timeToRead = getTimeToRead(words)
+
+  return (
+    <Article>
+      <SEO title={frontmatter.title} article={true} description={excerpt} />
+      <h1 className="title">{frontmatter.title}</h1>
+      <div className="publication-hint">
+        Published on {frontmatter.date} · {timeToRead} minutes to read
+      </div>
+      <MDXRenderer>{body}</MDXRenderer>
+    </Article>
+  )
+}
+Template.propTypes = {
+  data: PropTypes.shape({
+    mdx: PropTypes.shape({
+      frontmatter: PropTypes.shape({
+        title: PropTypes.string,
+        date: PropTypes.string,
+      }),
+      body: PropTypes.string,
+      wordCount: PropTypes.shape({
+        words: PropTypes.number,
+      }),
+      excerpt: PropTypes.string,
+    }),
+  }),
+}
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { path: { eq: $slug } }) {
+      body
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+      wordCount {
+        words
+      }
+      excerpt
+    }
+  }
+`
+
+export default Template

358dc924b77d9e58f37c0c1b502c2d2705725c0e
GitHub Action CI <action@github.com>
2022-04-11 22:29:12 +0000

    Add changes

---
+++ component---src-pages-404-js-c9a2ebf6a1c687d3f8cf.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksylvaindenyse_me=self.webpackChunksylvaindenyse_me||[]).push([[883],{429:function(e,n,t){t.r(n);var s=t(6771),l=(t(7294),t(3431));var i=(0,s.Z)("div",{target:"e1s5p4zs0"})({name:"7eer2p",styles:"display:flex;flex-direction:column;align-items:center;justify-content:center;flex-grow:1;text-align:center"});n.default=function(){return(0,l.tZ)(i,null,(0,l.tZ)("h1",null,"404"),(0,l.tZ)("p",null,"Oops, seems like the page doesn't exist or the link is broken."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c9a2ebf6a1c687d3f8cf.js.map
